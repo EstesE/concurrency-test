f9517c041ac64a6871a72b9450fc63b83e634d38
Eric Estes <eric_estes@aspensquare.com>
2018-04-04 15:19:06 -0400

    Updated to use waitForProperty and a local api.

--- app/index/route.js
+++ app/index/route.js
@@ -1,29 +1,42 @@
 import Route from '@ember/routing/route';
-import { task } from 'ember-concurrency';
+import { task, waitForProperty } from 'ember-concurrency';
 
 export default Route.extend({
+    posts: null,
+    post: null,
+
+    randomItem: function (items) {
+        return items[Math.floor(Math.random() * items.length)];
+    },
+
     model() {
         return {
-            properties: this.get('findPropertiesTask').perform(),
-            states: this.get('findStatesTask').perform(),
-            markets: this.get('findMarketsTask').perform()
+            posts: this.get('findPostsTask').perform(),
+            post: this.get('findPostTask').perform(),
+            comments: this.get('findCommentsTask').perform(),
+            tags: this.get('findTagsTask').perform()
         }
     },
 
-    findPropertiesTask: task(function * () {
-        return yield fetch('http://www.mocky.io/v2/5ac3eddb3000006600f47147?mocky-delay=6000ms').then(response => {
+    findPostsTask: task(function * () {
+        return yield fetch(`http://localhost:3000/posts?_delay=1000`).then(response => {
             return response.json();
         });
     }),
 
-    findStatesTask: task(function * () {
-        return yield fetch('http://www.mocky.io/v2/5ac3ed733000004900f47146?mocky-delay=2000ms').then(response => {
+    findPostTask: task(function * () {
+        yield waitForProperty(this, 'findPostsTask.isIdle');
+        return this.randomItem(this.get('findPostsTask.last.value'));
+    }),
+
+    findCommentsTask: task(function * () {
+        return yield fetch(`http://localhost:3000/comments?_delay=3000`).then(response => {
             return response.json();
         });
     }),
 
-    findMarketsTask: task(function * () {
-        return yield fetch('http://www.mocky.io/v2/5ac3ec5c3000005000f47145?mocky-delay=3000ms').then(response => {
+    findTagsTask: task(function * () {
+        return yield fetch(`http://localhost:3000/tags?_delay=2000`).then(response => {
             return response.json();
         });
     })
--- tests/integration/components/my-posts/component-test.js
+++ tests/integration/components/my-posts/component-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | my-markets', function(hooks) {
+module('Integration | Component | my-posts', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{my-markets}}`);
+    await render(hbs`{{my-posts}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#my-markets}}
+      {{#my-posts}}
         template block text
-      {{/my-markets}}
+      {{/my-posts}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
